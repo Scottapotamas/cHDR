7677694fd7cb576e0926b8f3ea35926aa2f671cd
Scott Rapson <scottapotamas@gmail.com>
2015-10-23 18:14:41 +1030

    Frame layout, 3x2 grid of images with margin for easily explained output... Text overplayed on frames to label them Uses same camera feed for all blocks

--- src/testing/oCV tests/cHDR.cpp
+++ src/testing/oCV tests/cHDR.cpp
@@ -26,8 +26,8 @@ int countCameras()
 int main( int argc, const char** argv )
 {
 
-    cout << "Counting cameras" << endl;
-    cout << countCameras() << endl;
+    //cout << "Counting cameras" << endl;
+    //cout << countCameras() << endl;
 
 
     VideoCapture cap0(0); // open the default camera
@@ -35,16 +35,22 @@ int main( int argc, const char** argv )
         cout << "Cannot find camera 0" << endl;
         return -1;
     } else {
-        cout << "Camera 0 has no issue" << endl;
+        cout << "Camera 0 has no issue. Setting resolution..." << endl;
+        
+        cap0.set(CV_CAP_PROP_FRAME_WIDTH,480);
+        cap0.set(CV_CAP_PROP_FRAME_HEIGHT,320);
     }
 
-    VideoCapture cap1(1); 
-    if(!cap1.isOpened()) {
-        cout << "Cannot find camera 1" << endl;
-        return -1;
-    } else {
-        cout << "Camera 1 has no issue" << endl;
-    }
+    // VideoCapture cap1(1); 
+    // if(!cap1.isOpened()) {
+    //     cout << "Cannot find camera 1" << endl;
+    //     return -1;
+    // } else {
+    //     cout << "Camera 1 has no issue. Setting resolution..." << endl;
+    //     cap1.set(CV_CAP_PROP_FRAME_WIDTH,640);
+    //     cap1.set(CV_CAP_PROP_FRAME_HEIGHT,480);
+    // }
+
     // VideoCapture cap2(0); 
     // if(!cap2.isOpened()) {
     //     cout << "Cannot find camera 2" << endl;
@@ -54,24 +60,66 @@ int main( int argc, const char** argv )
     // }    
 
 
-    Mat edges;
-    namedWindow("edges",1);
+//    Mat edges;
+//    namedWindow("edges",1);
      
     cout << "About to loop..." << endl;
 
-   while(1)
-   {
-    Mat frame0;
-    cap0 >> frame0; 
-    Mat frame1;
-    cap1 >> frame1; 
 
-    //imshow("webcam", frame0);
-    imshow("webcam1", frame1);
 
-     if(waitKey(30) >= 0)   //esc key is pressed for 30 seconds.
-        break;
+   while(1)   {
     
-    }
-}
+    // read an image
+    Mat image1;
+    cap0 >> image1;
+    
+    Mat image2;
+    cap0 >> image2;    
+    
+    Mat image3;
+    cap0 >> image3;  
 
+    Mat hdr;
+    cap0 >> hdr;  
+
+    Mat gamma;
+    cap0 >> gamma;
+
+    putText(image1, "RAW INPUT 1", Point(image1.cols/2 - 100,image1.rows-30), CV_FONT_NORMAL, 1, Scalar(255,255,255),1,1);
+    putText(image2, "RAW INPUT 2", Point(image1.cols/2 - 100,image1.rows-30), CV_FONT_NORMAL, 1, Scalar(255,255,255),1,1);
+    putText(image3, "RAW INPUT 3", Point(image1.cols/2 - 100,image1.rows-30), CV_FONT_NORMAL, 1, Scalar(255,255,255),1,1);
+    putText(hdr, "MERGED", Point(image1.cols/2 - 100,image1.rows-30), CV_FONT_NORMAL, 1, Scalar(255,255,255),1,1);
+    putText(gamma, "GAMMA", Point(image1.cols/2 - 100,image1.rows-30), CV_FONT_NORMAL, 1, Scalar(255,255,255),1,1);
+
+    int margin = 5;
+    int dstWidth = margin + image1.cols + margin + image2.cols + margin + image3.cols + margin;
+    int dstHeight = margin + image1.rows + margin*2 + hdr.rows + margin;
+
+    Mat dst = Mat(dstHeight, dstWidth, CV_8UC3, Scalar(0,0,0));
+
+
+    Mat targetROI = dst(Rect(margin, margin, image1.cols, image1.rows));
+    image1.copyTo(targetROI);
+
+    targetROI = dst(Rect(image1.cols + (margin*2), margin, image2.cols, image2.rows));
+    image2.copyTo(targetROI);
+
+    targetROI = dst(Rect(image2.cols + image3.cols + (margin*3), margin, image3.cols, image3.rows));
+    image3.copyTo(targetROI);
+
+    targetROI = dst(Rect(margin, image1.rows + margin*2, hdr.cols, hdr.rows));
+    hdr.copyTo(targetROI);
+
+    targetROI = dst(Rect(image2.cols + image3.cols + (margin*3), image1.rows + margin*2, gamma.cols, gamma.rows));
+    gamma.copyTo(targetROI);
+
+    
+    namedWindow("cHDR Test Window"); // create image window named "My Image"
+    imshow("cHDR Test Window", dst); // show the image on window
+
+    if(waitKey(30) >= 0)   //esc key is pressed for 30msec.
+        break;
+    }
+
+    return 0;
+}
