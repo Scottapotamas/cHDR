70ced9a91ae19e369dc20ba8926b404e2f7fe3ba
Scott Rapson <scottapotamas@gmail.com>
2015-10-30 23:05:38 +1030

    Runtime checks for platform openGL support, additional openCV imports

--- src/testing/oCV tests/cHDR.cpp
+++ src/testing/oCV tests/cHDR.cpp
@@ -1,11 +1,37 @@
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
- 
+
 #include <iostream>
 #include <stdio.h>
 
+#ifdef WIN32
+    #define WIN32_LEAN_AND_MEAN 1
+    #define NOMINMAX 1
+    #include <windows.h>
+#endif
+#if defined(_WIN64)
+    #include <windows.h>
+#endif
+
+#if defined(__APPLE__)
+    #include <OpenGL/gl.h>
+    #include <OpenGL/glu.h>
+#else
+    #include <GL/gl.h>
+    #include <GL/glu.h>
+#endif
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/core/opengl.hpp"
+#include "opencv2/core/cuda.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+ 
 using namespace std;
 using namespace cv;
+
+
+
 
 
 
