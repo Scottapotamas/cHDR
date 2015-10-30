fd2e58444b08b2352d21b92199ff8b9efc5fc80a
Scott Rapson <scottapotamas@gmail.com>
2015-10-31 00:59:53 +1030

    Clipping detection shader ported to processing sketch

--- src/testing/processing test/offline_shader/fragment.glsl
+++ src/testing/processing test/offline_shader/fragment.glsl
@@ -1,7 +1,5 @@
 
 uniform sampler2D textureSampler;
-
-// The inverse of the texture dimensions along X and Y
 uniform vec2 texOffset;
 varying vec4 vertColor;
 varying vec4 vertTexCoord;
@@ -10,47 +8,100 @@ uniform float time;
 uniform vec2 resolution;
 uniform vec2 mouse;
 
+uniform sampler2D iChannel0;
+uniform sampler2D iChannel1;
+uniform sampler2D iChannel2;
+
+
+
 // Layer between Processing and Shadertoy uniforms
 vec3 iResolution = vec3(resolution,0.0);
 float iGlobalTime = time;
 vec4 iMouse = vec4(mouse,0.0,0.0); // zw would normally be the click status
 vec4 fragCoord = gl_FragCoord.xyzw;
 
-#define halfPhase 3.5
-#define speed_modifier 1.5
+//-----------------------------------------------------
+
+#define mixsoft 0.1   
+#define mixrange 0.6
+
+
+//#define CLIP_SHOW 
+ 
+
+vec4 lumaKey(float threshH, float threshL, vec3 tex){
+    
+  vec4 buffer = vec4(tex, 1.0);
+    
+  float f = mixsoft/2.0;
+  float a = mixrange - f;
+  float b = mixrange + f;
+  
+    float luma = (tex.x + tex.y + tex.z) / 3.0;   //construct a grayscale from channel averages
+
+  float mask = smoothstep(a, b, luma);      //apply thresholding to luma map
+    
+    //if the abs luma value is above/below we have clipped data and should filter it out.
+    if(luma > threshH) {      
+        #ifdef CLIP_SHOW
+          buffer = vec4(1.0,0.0,0.0, 0.0);   
+        #else
+          buffer = vec4(0.0);   
+        #endif
+    } 
+    
+    if(luma < threshL) {      
+        #ifdef CLIP_SHOW
+          buffer = vec4(0.0,0.0,1.0, 0.0);   
+        #else
+          buffer = vec4(0.0);   
+        #endif
+    }
+    
+    return buffer;  //return the texture with transparent sections for clipped data
+}
 
 void main() {
 
-  float activeTime = iGlobalTime * speed_modifier;
-  vec3 col; 
-  float timeMorph = 0.0;
-  
-  #ifdef IS_IOS 
-    vec2 p = -1.0 + 2.0 * fragCoord.xy; //iOS Dynamic AR App support
-  #else
-    vec2 p = -1.0 + 2.0 * fragCoord.xy / iResolution.xy;
-  #endif
-  
-  p *= 7.0;
-  
-  float a = atan(p.y,p.x);
-  float r = sqrt(dot(p,p));
-  
-  if(mod(activeTime, 2.0 * halfPhase) < halfPhase)
-    timeMorph = mod(activeTime, halfPhase);
-  else
-    timeMorph = (halfPhase - mod(activeTime, halfPhase)); 
+  vec2 uv = fragCoord.xy / iResolution.xy * vec2(1.0,-1.0) + vec2(0.0, 1.0);
+  vec2 m = iMouse.xy/iResolution.xy;
+
+  //input textures from webcam(s)
+  vec3 texLow = texture2D(iChannel0, uv).xyz;
+  vec3 texMid = texture2D(iChannel1, uv).xyz;
+  vec3 texHigh = texture2D(iChannel2, uv).xyz;
+
+  float threshH = 0.85;
+  float threshL = 0.08;
     
-  timeMorph = 2.0*timeMorph + 1.0;
+  vec4 buffer = vec4(texMid, 1.0);
+    
+  float f = mixsoft/2.0;
+  float a = mixrange - f;
+  float b = mixrange + f;
   
-  float w = 0.25 + 3.0*(sin(activeTime + 1.0*r)+ 3.0*cos(activeTime + 5.0*a)/timeMorph);
-  float x = 0.8 + 3.0*(sin(activeTime + 1.0*r)+ 3.0*cos(activeTime + 5.0*a)/timeMorph);
+  float luma = (texMid.x + texMid.y + texMid.z) / 3.0;    //construct a grayscale from channel averages
+
+  float mask = smoothstep(a, b, luma);      //apply thresholding to luma map
+    
+    //if the abs luma value is above/below we have clipped data and should filter it out.
+    if(luma > threshH) {      
+        #ifdef CLIP_SHOW
+          buffer = vec4(1.0,0.0,0.0, 1.0);   
+        #else
+          buffer = vec4(texHigh, 1.0);   
+        #endif
+    } 
+    
+    if(luma < threshL) {      
+        #ifdef CLIP_SHOW
+          buffer = vec4(0.0,0.0,1.0, 1.0);   
+        #else
+          buffer = vec4(texLow, 1.0);   
+        #endif
+    }
+        
+  vec4 tempout = buffer;
   
-  col = vec3(0.1,0.2,0.82)*1.1;
-
-  gl_FragColor = vec4(col*w*x,1.0);
-
-
-  //vec2 uv = gl_FragCoord.xy / resolution.xy;
-  //gl_FragColor = vec4(uv,0.5+0.5*sin(iGlobalTime),1.0);
+  gl_FragColor = tempout;
 }
--- src/testing/processing test/offline_shader/offline_shader.pde
+++ src/testing/processing test/offline_shader/offline_shader.pde
@@ -1,6 +1,6 @@
 PImage cam0;
-//PImage cam1;
-//PImage cam2;
+PImage cam1;
+PImage cam2;
 
 PShader shader;  
 PImage img;
@@ -8,32 +8,33 @@ boolean enabled = true;
 
 void setup() {
  size(720, 480, P2D);
+ //frameRate(30);
 
  cam0 = loadImage("dark.JPG");
- //cam1 = loadImage("normal.JPG");
- //cam2 = loadImage("light.JPG");
+ cam1 = loadImage("normal.JPG");
+ cam2 = loadImage("light.JPG");
   
  shader = loadShader("fragment.glsl");
-  
+  //shaders.set("fraction", 1.0);
+
+ //uniforms
  shader.set("resolution", float(width), float(height));   
- //shaders.set("fraction", 1.0);
- //shaders.set("texture", cam0);
- //frameRate(30);
+ shader.set("testVar", 0.2);   
+ shader.set("iChannel0", cam0);
+ shader.set("iChannel1", cam1);
+ shader.set("iChannel2", cam2);
 
 }
 
 void draw() {
-  
 //  image(cam0, 0, 0, 720, 480);
 //  image(cam1, 0, 480, 720, 480);
 //  image(cam2, 0, 960, 720, 480);
- //shaders.set("time", millis() / 1000.0);
+ shader.set("time", millis() / 1000.0);  //update time sent to shader
+ shader(shader);
 
- if (enabled == true) {
-   shader(edges);
- }
  image(cam0, 0, 0);
-  
+
  if (frameCount % 10 == 0) {  // every 10th frame
    println("frame: " + frameCount + " - fps: " + frameRate);
  }
