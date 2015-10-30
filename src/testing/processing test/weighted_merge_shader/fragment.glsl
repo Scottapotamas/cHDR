f6a8bd860abe8cdaa6a8ab12440dc05c96934ea6
Scott Rapson <scottapotamas@gmail.com>
2015-10-31 01:14:20 +1030

    weighted HDR processing project

---
+++ src/testing/processing test/weighted_merge_shader/fragment.glsl
@@ -0,0 +1,94 @@
+
+uniform sampler2D textureSampler;
+uniform vec2 texOffset;
+varying vec4 vertColor;
+varying vec4 vertTexCoord;
+
+uniform float time;
+uniform vec2 resolution;
+uniform vec2 mouse;
+
+uniform sampler2D iChannel0;
+uniform sampler2D iChannel1;
+uniform sampler2D iChannel2;
+
+
+
+// Layer between Processing and Shadertoy uniforms
+vec3 iResolution = vec3(resolution,0.0);
+float iGlobalTime = time;
+vec4 iMouse = vec4(mouse,0.0,0.0); // zw would normally be the click status
+vec4 fragCoord = gl_FragCoord.xyzw;
+
+//-----------------------------------------------------
+
+float luminance(vec3 color)
+{
+    // Assuming that input color is in linear sRGB color space.
+    return (color.r * 0.2126) + 
+           (color.g * 0.7152) + 
+           (color.b * 0.0722);
+}
+
+float weight(float val)
+{
+    // Uses a tent function.
+    float w;
+
+    if (val <= 0.5)
+    {
+        w = val * 2.0;
+    }
+    else
+    {
+        w = (1.0 - val) * 2.0;
+    }
+
+    return w;
+}
+
+void main() {
+
+
+  vec2 uv = fragCoord.xy / iResolution.xy * vec2(1.0,-1.0) + vec2(0.0, 1.0);
+
+  const int refId = 1;                        ///< LDR reference image id (image with exposure = 1)
+  float weightSum = 0.0;                      ///< Sum of all weights
+  vec4 hdr        = vec4(0.0, 0.0, 0.0, 0.0); ///< Destination HDR image (last channel stores log luminance)
+
+  float lum;
+  float w;
+  float exposure;
+  vec3 ldr;
+  
+  
+  ldr = texture2D(iChannel0, uv).xyz;
+  lum      = luminance(ldr);
+  w        = weight(lum);
+  exposure = pow(2.0, float(0 - refId));
+  hdr.rgb       += (ldr / exposure) * w;
+  weightSum     += w;
+
+  
+  ldr = texture2D(iChannel1, uv).xyz;
+  lum      = luminance(ldr);
+  w        = weight(lum);
+  exposure = pow(2.0, float(1 - refId));
+  hdr.rgb       += (ldr / exposure) * w;
+  weightSum     += w;
+  
+  
+  ldr = texture2D(iChannel2, uv).xyz;
+
+  lum      = luminance(ldr);
+  w        = weight(lum);
+  exposure = pow(2.0, float(2 - refId));
+  hdr.rgb       += (ldr / exposure) * w;
+  weightSum     += w;
+  
+  
+  hdr.rgb /= weightSum + 1e-6;    
+  hdr.a = log(luminance(hdr.rgb) + 1e-6);
+
+  gl_FragColor = vec4(ldr,1.0);//hdr;
+}
---
+++ src/testing/processing test/weighted_merge_shader/weighted_merge_shader.pde
@@ -0,0 +1,42 @@
+PImage cam0;
+PImage cam1;
+PImage cam2;
+
+PShader shader;  
+PImage img;
+boolean enabled = true;
+
+void setup() {
+ size(720, 480, P2D);
+ //frameRate(30);
+
+ cam0 = loadImage("dark.JPG");
+ cam1 = loadImage("normal.JPG");
+ cam2 = loadImage("light.JPG");
+  
+ shader = loadShader("fragment.glsl");
+  //shaders.set("fraction", 1.0);
+
+ //uniforms
+ shader.set("resolution", float(width), float(height));   
+ shader.set("time", millis() / 1000.0);  //time in seconds since start
+ shader.set("iChannel0", cam0);
+ shader.set("iChannel1", cam1);
+ shader.set("iChannel2", cam2);
+
+}
+
+void draw() {
+//  image(cam0, 0, 0, 720, 480);
+//  image(cam1, 0, 480, 720, 480);
+//  image(cam2, 0, 960, 720, 480);
+ shader.set("time", millis() / 1000.0);  //update time sent to shader
+ shader(shader);
+
+ image(cam0, 0, 0);
+
+ if (frameCount % 10 == 0) {  // every 10th frame
+   println("frame: " + frameCount + " - fps: " + frameRate);
+ }
+
+}
